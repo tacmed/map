332f56ff5c724356f2ee70b92e0a7e17586f0811
Alexandr <samurayiii1986@gmail.com>
2023-05-01 23:15:47 +0300

    v85

--- js/fn.js
+++ js/fn.js
@@ -18,21 +18,21 @@ window.openHelp = function (hash, id) {
     const element_complete = document.querySelector("#modal-window_complete-button");
     const module_status = window._status_module[hash];
     
-    window._status_module[hash].id = id;
-
-    if (module_status.complete === true) {
-        element_repeat.style["display"] = "";
-        element_complete.style["display"] = "none";
-    } else {
-        element_complete.style["display"] = "";
-        element_repeat.style["display"] = "none";
-    }      
+    window._status_module[hash].id = id;     
 
     fetch(`modals/${hash}.html`).then((response) => {
 
         if (response.status >= 400) {
             return;
         }
+
+        if (module_status.complete === true) {
+            element_repeat.style["display"] = "";
+            element_complete.style["display"] = "none";
+        } else {
+            element_complete.style["display"] = "";
+            element_repeat.style["display"] = "none";
+        } 
 
         response.text().then( (data) => {
             (document.querySelector("#modal-window_modal-body")).innerHTML = data;
@@ -48,14 +48,19 @@ window.openHelp = function (hash, id) {
 
 };
 
-window.completeModule = function () {
+window.completeModule = function (id, hash) {
 
-    window._status_module[window._selected_hash].complete = true;
+    if (id === undefined && hash === undefined) {
+        hash = window._selected_hash;
+        id = window._selected_id;
+    }
+
+    window._status_module[hash].complete = true;
 
     const element_repeat = document.querySelector("#modal-window_repeat-button");
     const element_complete = document.querySelector("#modal-window_complete-button");
-    const element_kube = document.querySelector(`#${window._selected_id}-kube`);
-    const element_text = document.querySelector(`#${window._selected_id}-text`);
+    const element_kube = document.querySelector(`#${id}-kube`);
+    const element_text = document.querySelector(`#${id}-text`);
 
     element_repeat.style["display"] = "";
     element_complete.style["display"] = "none";
@@ -63,16 +68,25 @@ window.completeModule = function () {
     element_kube.setAttribute("stroke", "#36393d");
     element_kube.setAttribute("fill", "#b3b3b3");
 
+    if (localStorage !== undefined) {
+        localStorage[hash] = JSON.stringify(window._status_module[hash]);
+    }
+
 };
 
-window.repeatModule = function () {
+window.repeatModule = function (id, hash) {
 
-    window._status_module[window._selected_hash].complete = false;
+    if (id === undefined && hash === undefined) {
+        hash = window._selected_hash;
+        id = window._selected_id;
+    }
+
+    window._status_module[hash].complete = false;
 
     const element_repeat = document.querySelector("#modal-window_repeat-button");
     const element_complete = document.querySelector("#modal-window_complete-button");
-    const element_kube = document.querySelector(`#${window._selected_id}-kube`);
-    const element_text = document.querySelector(`#${window._selected_id}-text`);
+    const element_kube = document.querySelector(`#${id}-kube`);
+    const element_text = document.querySelector(`#${id}-text`);
 
     element_complete.style["display"] = "";
     element_repeat.style["display"] = "none";
@@ -80,9 +94,20 @@ window.repeatModule = function () {
     element_kube.setAttribute("stroke", element_kube.getAttribute("_stroke"));
     element_kube.setAttribute("fill", element_kube.getAttribute("_fill"));
 
+    if (localStorage !== undefined) {
+        localStorage[hash] = JSON.stringify(window._status_module[hash]);
+    }
+
 };
 
 document.addEventListener("DOMContentLoaded", () => {
+
+    if (localStorage !== undefined) {
+        const keys = Object.keys(localStorage);
+        for (const key of keys) {
+            window._status_module[key] = JSON.parse(localStorage.getItem(key));
+        }
+    }
 
     const svg_element = document.querySelector("#svg-schema");
     const container_element = document.querySelector("#container");
@@ -106,6 +131,15 @@ document.addEventListener("DOMContentLoaded", () => {
     if (svg_width < client_width) {
         const margin_left_svg = parseInt((client_width-svg_width)/2);
         svg_element.style["margin-left"] = `${margin_left_svg}px`;
+    }
+
+    for (const hash in window._status_module) {
+        const item = window._status_module[hash];
+        if (item.complete === true) {
+            completeModule(item.id, hash);
+        } else {
+            repeatModule(item.id, hash);
+        }
     }
 
     svg_element.style["display"] = "block";
